26a00547fbdcdea234c799fe304b8e169d02b11c
nicahgaturi <nicahgaturi@gmail.com>
2025-07-16 16:45:51 +0300

    email notification to admin after  generating a new range

--- PaleoCat/settings.py
+++ PaleoCat/settings.py
@@ -132,4 +132,18 @@ LOGIN_REDIRECT_URL = "PaleoApp:dashboard"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Logs email to terminal
+DEFAULT_FROM_EMAIL = 'no-reply@paleocat.local'
+
+# ---  (for production / testing real email) ---
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'email@gmail.com'
+# EMAIL_HOST_PASSWORD = 'app_password'
+# DEFAULT_FROM_EMAIL = 'PaleoCat <email@gmail.com>'
+
+
+
 
